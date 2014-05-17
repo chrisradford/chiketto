94fa405668ea52ec67efda37d21496b7166a8b11
Chris Radford <chris@chrisradford.com>
2014-05-17 02:25:01 +0100

    Removing unneeded test failure message

--- test/event_test.rb
+++ test/event_test.rb
@@ -4,8 +4,7 @@ class EventTest < MiniTest::Test
   def test_fetching_event
     VCR.use_cassette 'eventbriteapi' do
       event = Chiketto::Event.find 9203575141
-      assert_equal event.id, '9203575141',
-        "Event did not have the correct id: #{event.id}"
+      assert_equal event.id, '9203575141'
     end
   end
 end
